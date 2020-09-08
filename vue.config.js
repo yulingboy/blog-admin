4f9472536536d693d3e441ab6682c00f508ac612
yulingboy <yulinglsy@163.com>
2020-09-08 16:57:45 +0800

    第三次提交

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ module.exports = {
   //Solution For Issue:You are using the runtime-only build of Vue where the template compiler is not available. Either pre-compile the templates into render functions, or use the compiler-included build.
   //zhengkai.blog.csdn.net
   runtimeCompiler: true,
+  devServer: {
+    disableHostCheck: true
+  },
   configureWebpack: {
     resolve: {
       alias: {
