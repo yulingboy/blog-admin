dfd62deae6702b7f0a516a53d0ff5ee576bc591d
yulingboy <yulinglsy@163.com>
2020-09-09 11:07:08 +0800

    第十三次提交

--- vue.config.js
+++ vue.config.js
@@ -6,8 +6,8 @@ module.exports = {
     disableHostCheck: true,
     headers: {
       'Access-Control-Allow-Origin': '*'
-    },
-    host: '127.0.0.1'
+    }
+    // host: '112.124.203.100'
     // port: '3000'
   },
   configureWebpack: {
